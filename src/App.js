857d34a63c1236b5fa48eefd99fead6e460bc9ae
jennawills <jenna.wills321@gmail.com>
2019-04-12 22:33:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Project from './Project.js';
+import Task from "./Taskboard.jpg";
+import Uv from "./uvtracknew.jpg";
+import Hoa from "./hoa.jpg";
+
+
+    var projects = [
+      {id: 0, name: "Task Board", show:false, image:Task, description: "Built using React, this is a task board based on a Kanban board. Create a new card, add or remove a task item."},
+      {id: 1, name: "HOA", image:Hoa,show:false, description: "Built using React, this is a sample site for a community homeowner association. Uses routing to navigate to additional site pages."},
+      {id: 2, name: "UV Index App", image:Uv, show:false, description: "Built using React, a single page app that uses API from OpenUV to fetch realtime UV index. Also, as a fun feature, user is able to choose from different backgrounds."},
+    //  {id: 3, name: "Arcade Game"},
+    //  {id: 4, name: "Neighborhood App"},
+    //  {id: 5, name: "Memory Game"}
+    ];
+
+
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.scrollDiv = React.createRef();
+      this.scrollDivProj = React.createRef();
+      this.state = {projects}
+  }
+
+
   render() {
+
+    var toggleProject = (event) => {
+       var newProjects = [...this.state.projects];
+
+       newProjects.map(item => Number(item.id) === Number(event.target.id) &&  item.show === false? item.show = true : item.show = false)
+
+       this.setState({projects:newProjects})
+
+
+   }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="headerTitle"> Jenna Michele </div>
+          <nav>
+            <ul className="navList">
+              <li className="navListItem" onClick={() => {
+              this.scrollDiv.current.scrollIntoView({ behavior: 'smooth' });
+            }}> About Me </li>
+              <li className="navListItem" onClick={() => {
+              this.scrollDivProj.current.scrollIntoView({ behavior: 'smooth' });
+            }}> Projects </li>
+              <li className="navListItem"> Contact Me </li>
+            </ul>
+          </nav>
         </header>
+        <main className="mainContent">
+
+          <div className="projectContainer" ref={this.scrollDivProj}>
+          <h1 className="projectHeader"> Projects </h1>
+            {this.state.projects.map(item => <Project image={item.image} id={item.id} show={item.show} summary={item.description} toggleProject={toggleProject} />)}
+          </div>
+         <div className="aboutSection" >
+            <h1 className="aboutHeader"> About Me </h1>
+            <p> Welcome! As you probably guessed, my name is Jenna. I am passionate about
+              web development and design. I began my coding journey with Udacity
+              and have been hooked since. I enjoy bringing design to life with
+              javascript and continually learning from others. My other passion
+              is health and creating apps that can improve my family's health,
+              which inspired my project to track UV exposure! I hope to work
+              with you soon! </p>
+
+         </div>
+         <div className="anchor" ref={this.scrollDiv} />
+        </main>
       </div>
     );
   }
